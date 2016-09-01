b5c4dba34ecb0fb02e01ac724d13eba161b84092
Shawn <hufeihxw@gmail.com>
2016-09-01 22:27:41 +0800

    added file/url loader. add styles to html

--- src/server/page/Html.js
+++ src/server/page/Html.js
@@ -4,6 +4,7 @@ import {renderToString} from 'react-dom/server';
 
 import { Provider } from 'react-fela'
 import felaRenderer from '../../universal/felaRenderer'
+import App from '../../universal/app'
 
 
 const renderer = felaRenderer()
@@ -19,21 +20,28 @@ export default class Html extends Component {
   render() {
     const PROD = process.env.NODE_ENV === 'production';
     const {title} = this.props;
+
+    const htmlMarkup = renderToString(
+      <App renderer={renderer} />
+    )
     // const appHtml = renderToString(
     //   <Provider renderer={renderer}>
     //   </Provider>
     // );
     const scripts = Object.entries(this.props.chunks.javascript).map(([name, value])=><script key={name} src={value}/>)
+    const styles = Object.entries(this.props.chunks.styles).map(([name, value])=><style key={name} src={value}/>)
     return (
       <html>
       <head>
-        <meta charSet="utf-8"/>
+        <meta charSet='utf-8'/>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
         <title>{title}</title>
-        <style id="stylesheet"/>
-        <style id="font-stylesheet"/>
+        <style id='stylesheet'/>
+        <style id='font-stylesheet'/>
+        {styles}
       </head>
       <body>
-      <div id="root"></div>
+      <div id='root'>{htmlMarkup}</div>
       {PROD && <script src={vendor.js}/>}
       {scripts}
       </body>
